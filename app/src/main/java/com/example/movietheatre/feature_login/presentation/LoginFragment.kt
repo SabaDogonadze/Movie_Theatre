5a61ca7bfc0de019902f863649190449793bbf32
DatoKochla18 <shalvakochlamazashvili2@gmail.com>
2025-03-24 14:31:41 +0400

    dummy changes

--- app/src/main/java/com/example/movietheatre/feature_login/presentation/LoginFragment.kt
+++ app/src/main/java/com/example/movietheatre/feature_login/presentation/LoginFragment.kt
@@ -2,11 +2,12 @@ package com.example.movietheatre.feature_login.presentation
 
 import com.example.movietheatre.core.presentation.BaseFragment
 import com.example.movietheatre.databinding.FragmentLoginBinding
+import com.example.movietheatre.feauture_home.presentation.Dato
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
     override fun setUp() {
-        TODO("Not yet implemented")
+        println(Dato.name)
     }
 
     override fun clickListeners() {
