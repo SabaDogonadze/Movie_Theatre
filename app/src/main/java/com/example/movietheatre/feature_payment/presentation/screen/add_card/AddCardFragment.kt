9d421eb9ca768a1b0c03d469ed95d2433ab50055
DatoKochla18 <shalvakochlamazashvili2@gmail.com>
2025-04-05 17:17:51 +0400

    nearly finished ui

---
+++ app/src/main/java/com/example/movietheatre/feature_payment/presentation/screen/add_card/AddCardFragment.kt
@@ -0,0 +1,15 @@
+package com.example.movietheatre.feature_payment.presentation.screen.add_card
+
+import com.example.movietheatre.core.presentation.BaseFragment
+import com.example.movietheatre.databinding.FragmentAddCardBinding
+
+class AddCardFragment : BaseFragment<FragmentAddCardBinding>(FragmentAddCardBinding::inflate) {
+    override fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun clickListeners() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/movietheatre/feature_payment/presentation/screen/payment/PaymentFragment.kt
@@ -0,0 +1,16 @@
+package com.example.movietheatre.feature_payment.presentation.screen.payment
+
+import com.example.movietheatre.core.presentation.BaseFragment
+import com.example.movietheatre.databinding.FragmentPaymentBinding
+
+
+class PaymentFragment : BaseFragment<FragmentPaymentBinding>(FragmentPaymentBinding::inflate) {
+    override fun setUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun clickListeners() {
+        TODO("Not yet implemented")
+    }
+
+}
